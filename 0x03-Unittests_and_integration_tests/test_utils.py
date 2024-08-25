85915366f8631ea14609d06b0693c7af1ddf47c1
aellun <okello.k.otieno@gmail.com>
2024-08-25 12:32:18 +0300

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -47,6 +48,45 @@ class TestAccessNestedMap(unittest.TestCase):
 
         # Verify that the exception message is as expected
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+
+class TestGetJson(unittest.TestCase):
+    ''''
+        Test suite for the get_json function from utils module.
+        This class contains unit tests that verify the functionality of
+        the get_json function using mocked HTTP responses.
+    '''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        '''
+            Test that get_json returns the expected result
+            from the mocked requests.get.
+            Args:
+                test_url (str): The URL to be passed to get_json.
+                test_payload (dict): The expected payload to be returned
+                mock_get (Mock): The mocked requests.get method.
+        '''
+        # Create a Mock response object with a json method
+        # that returns test_payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call the get_json function with the test_url
+        result = get_json(test_url)
+
+        # Assert that requests.get was called
+        # exactly once with the test_url
+        mock_get.assert_called_once_with(test_url)
+
+        # Assert that the result of get_json is
+        # equal to the test_payload
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
